50f488f26ba852a449f9372e271dc47b7362dbb6
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 20:46:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,6 @@ end
 def find_the_cheese(cheese_products)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese_products|
-    cheese_products.find 
+    cheese_products.find?(cheese_types)
+end
 end
