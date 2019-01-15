cc64c35d8eace324c1e0b1b7c52e3646c1131ee1
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 20:12:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,13 @@ def roll_call_dwarves(dwarves)
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i=0
+  captian_planet =[]
+  if i < array.length
+    captian_planet << array[i]
+    i+=1
+  end
 end
 
 def long_planeteer_calls# code an argument here
